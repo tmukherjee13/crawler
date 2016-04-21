a02ba8c15411a23dfa5b5933a59cee0c03b18948
Tarun M <tarun.mukherjee@indusnet.co.in>
2016-04-21 18:50:31 +0530

    initial commit

---
+++ src/crawl.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import urllib2
+import urlparse
+from bs4 import BeautifulSoup
+
+def getAllUrl(url):
+    try:
+        page = urllib2.urlopen( url ).read()
+    except:
+        return []
+    urlList = []
+    try:
+        soup = BeautifulSoup(page,"html.parser")
+        soup.prettify()
+        for anchor in soup.findAll('a', href=True):
+            if not 'http://' in anchor['href']:
+                if urlparse.urljoin(url, anchor['href']) not in urlList:
+                    urlList.append(urlparse.urljoin(url, anchor['href']))
+            else:
+                if anchor['href'] not in urlList:
+                    urlList.append(anchor['href'])
+
+        length = len(urlList)
+
+        return urlList
+    except urllib2.HTTPError, e:
+        print e
+
+def listAllUrl(urls):
+    for x in urls:
+        print x
+        urls.remove(x)
+        urls_tmp = getAllUrl(x)
+        for y in urls_tmp:
+            urls.append(y)
+
+
+if __name__ == "__main__":
+    urls = ['http://vaxia.loc']
+    while(urls.count>0):
+        urls = getAllUrl('http://vaxia.loc')
+        listAllUrl(urls)
